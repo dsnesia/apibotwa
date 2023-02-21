1e56c863d999875f04f3fb056eec960232c3617c
dsnesia <109241727+dsnesia@users.noreply.github.com>
2023-02-21 20:02:15 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.creator = 'DSnesis'// yourname
+global.MONGO_DB_URI = "mongodb+srv://b:1@cluster0.3rmvnpt.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
+global.ACTIVATION_TOKEN_SECRET = "BUPKI?" //isi apa aja bebas
+global.your_email = "darwantow758@gmail.com" //email
+global.email_password = "wpprdmrczduntuue" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
